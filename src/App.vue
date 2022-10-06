978fdb9fdbf8cf04fc99e222b4f93e120c3c2eed
Cristhian Uriel <a3521110258@alumno.uttehuacan.edu.mx>
2022-10-05 21:44:54 -0500

    Subiendo archivos
    subiendo archivos del proyecto

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container mt-3">
+       <Header />
+        
+        <formularios/>
+    </div>
+    
+</template>
+
+<script>
+    import Header from "./components/Header.vue";
+    import Formularios from "./components/Formularios.vue";
+
+    export default {
+        components:{
+          Header,
+          Formularios,
+        },
+    };
+</script>
